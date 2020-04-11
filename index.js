5b5ff0a789720668880b0f2f975671fe7319b6db
Alexandra Dumitrescu <lexidumitrescu@gmail.com>
2020-04-11 17:59:08 +0300

    add board

---
+++ index.js
@@ -0,0 +1,14 @@
+let board = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+function makeMove(board, player, x, y) {
+  return board;
+}
+function showBoard(board) {
+  console.log(board);
+}
+showBoard(board);
+board = makeMove(board, "A", 2, 2);
+showBoard(board);
